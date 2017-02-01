8e374d57c60d9624735ab887d18fe6c0321b495a
Stanislav Kutasevits <stan@bellcom.dk>
2017-02-01 16:46:08 +0200

    decreto menu collapsing

--- public_html/themes/decreto_theme/js/main.js
+++ public_html/themes/decreto_theme/js/main.js
@@ -6,21 +6,8 @@
 jQuery(function ($) {
 
     //main menu
-    //alert('her');
     $('#toggle-menu-trigger').click(function () {
-        //header-container
-        $('.header-container .toggle-menu-left').toggleClass('col-sm-1 col-sm-2');
-        $('.header-container .toggle-menu-right').toggleClass('col-sm-9 col-sm-10');
-
-        //main-container
-        $('.main-container .toggle-menu-left').toggleClass('col-sm-1 col-sm-2');
-        if ($('.main-container .toggle-menu-right').hasClass('col-sm-7') || $('.main-container .toggle-menu-right').hasClass('col-sm-8')) {
-            $('.main-container .toggle-menu-right').toggleClass('col-sm-7 col-sm-8');
-        }
-
-        if ($('.main-container .toggle-menu-right').hasClass('col-sm-10') || $('.main-container .toggle-menu-right').hasClass('col-sm-11')) {
-            $('.main-container .toggle-menu-right').toggleClass('col-sm-10 col-sm-11');
-        }
+        $('.toggle-menu-container').toggleClass('collapsed');
     });
 
     $("#main_menu ul li.has-children").hover(
