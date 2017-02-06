01b64e389714f8d3679829cdb42bf9ffd43809f8
Stanislav Kutasevits <stan@bellcom.dk>
2017-02-06 11:41:27 +0200

    moving bootstrap multiselect library

--- public_html/themes/decreto_theme/js/main.js
+++ public_html/themes/decreto_theme/js/main.js
@@ -20,4 +20,47 @@
             });
         }
     }
+
+    Drupal.behaviors.bootstrap_multiselect = {
+        attach: function (context, settings) {
+            var defaultTitle = 'Select';
+            $('.bootstrap-multiselect', context).once('bootstrap_multiselect').multiselect({
+                buttonText: function (options, select) {
+                    if (options.length === 0) {
+                        if ($(select).data('multiselect-title')) {
+                            return $(select).data('multiselect-title');
+                        }
+                        return defaultTitle;
+                    }
+                    else if (options.length > 1) {
+                        return 'Multiple selected';
+                    }
+                    else {
+                        var labels = [];
+                        options.each(function () {
+                            if ($(this).attr('label') !== undefined) {
+                                labels.push($(this).attr('label'));
+                            }
+                            else {
+                                labels.push($(this).html());
+                            }
+                        });
+                        return labels.join(', ') + '';
+                    }
+                }
+            });
+        }
+    }
+
+    Drupal.behaviors.main_container_offset = {
+        attach: function (context, settings) {
+            //offsetting main-container__content
+            $('.main-container__content', context).css('margin-top', $('.main-container__header', context).height());
+            $(window).once('main_container_offset').resize(function () {
+                $('.main-container__content', context).css('margin-top', $('.main-container__header', context).height());
+            });
+        }
+    }
+
+
 })(jQuery, Drupal);
