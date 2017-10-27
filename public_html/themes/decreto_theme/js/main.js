e474671a61db8bc04aed961dd321d29bd00f8e60
Rajat Sharma <rajatsharma@Rajats-MacBook-Air.local>
2017-10-27 10:24:43 +0200

    Decreto theme changes

--- public_html/themes/decreto_theme/js/main.js
+++ public_html/themes/decreto_theme/js/main.js
@@ -64,56 +64,48 @@
             });
         }
     }
-    /*-------------------------------------fgewg---------------------------*/
-   
+
+
 })(jQuery, Drupal);
+/*----------------------------caret-class------------------*/
 
 
+/*----------------------------toggleClassForPARTICIPANTS------------*/
 jQuery(document).ready(function($){
 
-
-$(".btn-primary").click(function(){
-    $(this).toggleClass("down"); 
+$(".field--name-field-decreto-meet-partic .field--label").click(function(){
+  $(".field--name-field-decreto-meet-partic .col-xs-7").toggle("slideUp");
+ });
 });
-});
-/*------------------------njhdbh*/
+/*----------------------------toggleClassForDESRIPTION------------*/
 jQuery(document).ready(function($){
 
-
-$("#toggle-menu-trigger").click(function(){
-    $('.toggle-menu-container').toggleClass('containerClose');
-    $('#superfish-main li').toggleClass('closeContainerMenuItems');
-    $('#toggle-menu-trigger').toggleClass("rotated");
-    $('#toggle-menu-trigger').toggleClass("visible");
-    $('.container-fluid').toggleClass('move');
-
-});
-});
-/*----------------------------                          */
-
-/*------------------subIndicatorsMainMenuForDesktop-------------*/
-jQuery(document).ready(function($){
-
-$(".sf-sub-indicator").click(function(){
-    
-    $('.sf-depth-1 .sf-depth-2 > a').toggleClass("open");
-    $('.sf-sub-indicator').toggleClass("rotate");
-});
+$(".field--type-text-with-summary .field--label").click(function(){
+  $(".field--type-text-with-summary .col-xs-7").toggle("slideUp");
+ });
 });
 
-/*----------------------------toggleClassMeetingsForParticipants------------*/
-jQuery(document).ready(function($){
 
-$('#info .field--name-field-decreto-meet-partic .field--label').on('click', function() {
-    $('#info .col-xs-7 #Participants').animate({
-        'height': 'toggle'
-    },0); });
-});
-/*----------------------------toggleClassMeetingsForDescription------------*/
-jQuery(document).ready(function($){
 
-$('#info .field--type-text-with-summary').on('click', function() {
-    $('#info .col-xs-7 #Description').animate({
-        'height': 'toggle'
-    }, 0); });
-});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
