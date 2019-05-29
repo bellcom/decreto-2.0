45e15f58816e8cb2e4f5a3fbb1d0cb0affb6e01b
Morten Nissen <mail@hjemmesidekongen.dk>
2019-05-29 15:19:43 +0200

    Mobile sidebar

--- web/themes/custom/custom_theme/src/javascripts/app.js
+++ web/themes/custom/custom_theme/src/javascripts/app.js
@@ -4,23 +4,12 @@ jQuery(function($) {
     // Flexy header
     flexy_header.init();
 
-    // Sidr
-    $('.slinky-menu')
-        .find('ul, li, a')
-        .removeClass();
-
     $('.sidr-toggle--right').sidr({
         name: 'sidr-main',
         side: 'right',
         renaming: false,
         body: '.layout__wrapper',
         source: '.sidr-source-provider'
-    });
-
-    // Slinky
-    $('.sidr .slinky-menu').slinky({
-        title: true,
-        label: ''
     });
 
     // Enable / disable Bootstrap tooltips, based upon touch events
