78c9dee62b312c43cd5edc1d197fdd1af7743516
Stanislav Kutasevits <stan@bellcom.dk>
2019-06-20 13:33:02 +0300

    DE-105 meeting create page, adding participants

--- web/modules/custom/decreto_content_modify/src/Form/MeetingsEditForm.php
+++ web/modules/custom/decreto_content_modify/src/Form/MeetingsEditForm.php
@@ -18,6 +18,7 @@ use Drupal\datetime\Plugin\Field\FieldType\DateTimeItemInterface;
 use Drupal\field\Entity\FieldStorageConfig;
 use Drupal\node\Entity\Node;
 use Drupal\node\NodeInterface;
+use Drupal\user\Entity\User;
 
 /**
  * Meeting create or edit form.
@@ -56,94 +57,52 @@ class MeetingsEditForm extends FormBase {
   public function buildForm(array $form, FormStateInterface $form_state, NodeInterface $meeting = NULL) {
     $this->meeting = $meeting;
 
+    $autofillParticipants = $form_state->get('autofill_participants');
+    if (!isset($autofillParticipants)) {
+      $autofillParticipants = TRUE;
+      $form_state->set('autofill_participants', $autofillParticipants);
+    }
+
+    $activePage = $form_state->get('active_page');
+    if (!isset($activePage)) {
+      $activePage = 1;
+      $form_state->set('active_page', $activePage);
+    }
+
     $form['#prefix'] = '<div id="' . $this->getFormId(). '">';
     $form['#suffix'] = '</div>';
 
-    // Type.
-    $type_options = options_allowed_values(FieldStorageConfig::loadByName('node', 'field_decreto_meet_type'));
-    $form['type'] = [
-      '#type' => 'select',
-      '#title' => $this->t('Type'),
-      '#options' => $type_options,
-      '#required' => TRUE,
+    // Steps container START.
+    $form['steps-container'] = [
+      '#type' => 'container',
+      '#attributes' => [
+        'class' => [($autofillParticipants)? 'hidden' : ''],
+      ],
     ];
-
-    $form['title'] = [
-      '#type' => 'textfield',
-      '#placeholder' => $this->t('Title'),
-      '#title' => $this->t('Title'),
-      '#description' => $this->t('Meeting full title'),
+    $form['steps-container']['steps-step-1'] = [
+      '#type' => 'html_tag',
+      '#tag' => 'span',
+      '#value' => $this
+          ->t('Step 1'),
+      '#attributes' => [
+        'class' => [($activePage === 1)? 'bg-primary' : ''],
+      ],
     ];
-
-    // Department.
-    $department_terms = \Drupal::service('entity_type.manager')
-      ->getStorage('taxonomy_term')
-      ->loadTree('decreto_tax_department');
-    $department_options = array();
-    foreach ($department_terms as $term) {
-      $department_options[$term->tid] = $term->name;
-    }
-    $form['department'] = [
-      '#type' => 'select',
-      '#title' => $this->t('Department'),
-      '#options' => $department_options,
-      '#required' => TRUE,
+    $form['steps-container']['steps-step-2'] = [
+      '#type' => 'html_tag',
+      '#tag' => 'span',
+      '#value' => $this
+          ->t('Step 2'),
+      '#attributes' => [
+        'class' => [($activePage === 2)? 'bg-primary' : ''],
+      ],
     ];
+    // Steps container END.
 
-    // Location.
-    $location_terms = \Drupal::service('entity_type.manager')
-      ->getStorage('taxonomy_term')
-      ->loadTree('decreto_tax_location');
-    $location_options = array();
-    foreach ($location_terms as $term) {
-      $location_options[$term->tid] = $term->name;
-    }
-    $form['location'] = [
-      '#type' => 'select',
-      '#title' => $this->t('Location'),
-      '#options' => $location_options,
-      '#required' => TRUE,
-    ];
-
-    // Automatically populate checkbox.
-    $form['populate_department_members'] = [
-      '#type' => 'checkbox',
-      '#title' => $this->t('Automatically populate members from selected department'),
-    ];
-
-    // Meeting dates.
-    $form['start_date'] = array(
-      '#type' => 'datetime',
-      '#title' => $this->t('Start date - Free choice'),
-      '#default_value' => DrupalDateTime::createFromTimestamp(time()),
-    );
-    $form['end_date'] = array(
-      '#type' => 'datetime',
-      '#title' => $this->t('End date - Free choice'),
-      '#default_value' => DrupalDateTime::createFromTimestamp(time()),
-    );
-
-    // TODO: do we need to upload PDF and save them as meeting description?
-//    $form['full_doc'] = array(
-//      '#title' => $this->t('Open description'),
-//      '#type' => 'managed_file',
-//      '#upload_location' => 'public://',
-//      '#default_value' => NULL,
-//      '#upload_validators' => array(
-//        'file_validate_extensions' => array('txt pdf doc docx'),
-//      )
-//    );
-//
-//    $form['full_doc_closed'] = array(
-//      '#title' => $this->t('Closed description'),
-//      '#type' => 'managed_file',
-//      //'#upload_location' => 'private://',
-//      '#upload_location' => 'public://', //TODO:change to private
-//      '#default_value' => NULL,
-//      '#upload_validators' => array(
-//        'file_validate_extensions' => array('txt pdf doc docx'),
-//      )
-//    );
+    // Adding pages START.
+    $form = $this->appendFormPage1($form, $form_state);
+    $form = $this->appendFormPage2($form, $form_state);
+    // Adding pages END.
 
 //    // If it is meeting's edit page, populate values.
 //    if ($meeting) {
@@ -165,11 +124,10 @@ class MeetingsEditForm extends FormBase {
 ////      }
 //    }
 
+    // Form actions START.
     $form['actions'] = [
       '#type' => 'actions',
     ];
-
-    // Cancel button.
     $form['actions']['cancel'] = [
       '#type' => 'button',
       '#value' => $this->t('Cancel'),
@@ -180,8 +138,18 @@ class MeetingsEditForm extends FormBase {
       ],
       '#limit_validation_errors' => [],
     ];
-
-    // Submit button.
+    $form['actions']['switch-page'] = [
+      '#type' => 'submit',
+      '#value' => ($activePage === 1)? $this->t('Go further') : $this->t('Go back'),
+      '#ajax' => [
+        'callback' => '::ajaxReloadForm',
+        'event' => 'click',
+      ],
+      '#submit' => ['::submitSwitchPage'],
+      '#attributes' => [
+        'class' => [($autofillParticipants)? 'hidden' : '']
+      ]
+    ];
     $form['actions']['submit'] = [
       '#type' => 'submit',
       '#value' => $this->t('Save'),
@@ -189,7 +157,239 @@ class MeetingsEditForm extends FormBase {
         'callback' => '::ajaxSubmitForm',
         'event' => 'click',
       ],
+      '#attributes' => [
+        // Show button only if we autofill participants, or if we are on the
+        // second page of the form.
+        'class' => [(!$autofillParticipants && $activePage !== 2)? 'hidden' : '']
+      ]
     ];
+    // Form actions END.
+
+    return $form;
+  }
+
+  /**
+   * Appends first page components to a form.
+   *
+   * @param array $form
+   *   Render array representing from.
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   *   Current form state.
+   *
+   * @return array
+   *    Form array with appended page.
+   */
+  private function appendFormPage1(array $form, FormStateInterface $form_state) {
+    $autofillParticipants = $form_state->get('autofill_participants');
+    $activePage = $form_state->get('active_page');
+
+    // Page 1 container.
+    $form['pages-page-1'] = [
+      '#type' => 'container',
+      '#attributes' => [
+        'class' => [($activePage !== 1)? 'hidden' : ''],
+      ],
+    ];
+
+    // Type.
+    $type_options = options_allowed_values(FieldStorageConfig::loadByName('node', 'field_decreto_meet_type'));
+    $form['pages-page-1']['type'] = [
+      '#type' => 'select',
+      '#title' => $this->t('Type'),
+      '#options' => $type_options,
+      '#required' => TRUE,
+    ];
+
+    $form['pages-page-1']['title'] = [
+      '#type' => 'textfield',
+      '#placeholder' => $this->t('Title'),
+      '#title' => $this->t('Title'),
+      '#required' => TRUE,
+      '#description' => $this->t('Meeting full title'),
+    ];
+
+    // Department.
+    $department_terms = \Drupal::service('entity_type.manager')
+      ->getStorage('taxonomy_term')
+      ->loadTree('decreto_tax_department');
+    $department_options = [];
+    foreach ($department_terms as $term) {
+      $department_options[$term->tid] = $term->name;
+    }
+    $form['pages-page-1']['department'] = [
+      '#type' => 'select',
+      '#title' => $this->t('Department'),
+      '#options' => $department_options,
+      '#required' => TRUE,
+    ];
+
+    // Location.
+    $location_terms = \Drupal::service('entity_type.manager')
+      ->getStorage('taxonomy_term')
+      ->loadTree('decreto_tax_location');
+    $location_options = [];
+    foreach ($location_terms as $term) {
+      $location_options[$term->tid] = $term->name;
+    }
+    $form['pages-page-1']['location'] = [
+      '#type' => 'select',
+      '#title' => $this->t('Location'),
+      '#options' => $location_options,
+      '#required' => TRUE,
+    ];
+
+    // Automatically populate checkbox.
+    // Must be of type submit in order to talk to backend via Ajax.
+    $form['pages-page-1']['populate_department_members'] = [
+      '#type' => 'submit',
+      '#value' => $this->t('Automatically populate members from selected department'),
+      '#ajax' => [
+        'callback' => '::ajaxReloadForm',
+        'progress' => [
+          'type' => 'none'
+        ]
+      ],
+      '#attributes' => [
+        // Example of altering button class depending on autofill mode status.
+        'class' => [($autofillParticipants)? 'btn-primary' : ''],
+      ],
+      '#submit' => ['::submitToggleParticipantsAutofill'],
+      '#limit_validation_errors' => [],
+    ];
+
+    // Meeting dates.
+    $form['pages-page-1']['start_date'] = [
+      '#type' => 'datetime',
+      '#title' => $this->t('Start date - Free choice'),
+      '#default_value' => DrupalDateTime::createFromTimestamp(time()),
+    ];
+    $form['pages-page-1']['end_date'] = [
+      '#type' => 'datetime',
+      '#title' => $this->t('End date - Free choice'),
+      '#default_value' => DrupalDateTime::createFromTimestamp(time()),
+    ];
+
+    // TODO: do we need to upload PDF and save them as meeting description?
+    //    $form['full_doc'] = array(
+    //      '#title' => $this->t('Open description'),
+    //      '#type' => 'managed_file',
+    //      '#upload_location' => 'public://',
+    //      '#default_value' => NULL,
+    //      '#upload_validators' => array(
+    //        'file_validate_extensions' => array('txt pdf doc docx'),
+    //      )
+    //    );
+    //
+    //    $form['full_doc_closed'] = array(
+    //      '#title' => $this->t('Closed description'),
+    //      '#type' => 'managed_file',
+    //      //'#upload_location' => 'private://',
+    //      '#upload_location' => 'public://', //TODO:change to private
+    //      '#default_value' => NULL,
+    //      '#upload_validators' => array(
+    //        'file_validate_extensions' => array('txt pdf doc docx'),
+    //      )
+    //    );
+
+    return $form;
+  }
+
+  /**
+   * Appends second page components to a form.
+   *
+   * @param array $form
+   *   Render array representing from.
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   *   Current form state.
+   *
+   * @return array
+   *    Form array with appended page.
+   */
+  private function appendFormPage2(array $form, FormStateInterface $form_state) {
+    $activePage = $form_state->get('active_page');
+
+    // Page 2 container.
+    $form['pages-page-2'] = [
+      '#type' => 'container',
+      '#attributes' => [
+        'class' => [($activePage !== 2)? 'hidden' : ''],
+      ],
+    ];
+
+    // Participants START.
+    $form['pages-page-2']['participants-container'] = [
+      '#type' => 'container',
+    ];
+    $form['pages-page-2']['participants-container']['header'] = [
+      '#type' => 'container',
+      '#attributes' => [
+        'class' => ['row'],
+      ],
+    ];
+    $form['pages-page-2']['participants-container']['header'][] = [
+      '#type' => 'html_tag',
+      '#tag' => 'div',
+      '#value' => $this->t('First and last name'),
+      '#attributes' => [
+        'class' => ['col-xs-6'],
+      ],
+    ];
+    $form['pages-page-2']['participants-container']['header'][] = [
+      '#type' => 'html_tag',
+      '#tag' => 'div',
+      '#value' => $this->t('Internal'),
+      '#attributes' => [
+        'class' => ['col-xs-3'],
+      ],
+    ];
+    $form['pages-page-2']['participants-container']['header'][] = [
+      '#type' => 'html_tag',
+      '#tag' => 'div',
+      '#value' => $this->t('External'),
+      '#attributes' => [
+        'class' => ['col-xs-3'],
+      ],
+    ];
+
+    $query = \Drupal::entityQuery('user')
+      ->condition('status', 1);
+    $users_ids = $query->execute();
+    if (!empty($users_ids)) {
+      $form['pages-page-2']['participants-container']['participants'] = [
+        '#type' => 'container',
+        '#tree' => TRUE,
+      ];
+      $users = User::loadMultiple($users_ids);
+
+      foreach($users as $user) {
+        $user_id = $user->id();
+        $form['pages-page-2']['participants-container']['participants'][$user_id] = [
+          '#type' => 'container',
+          '#attributes' => [
+            'class' => ['row'],
+          ],
+        ];
+        $form['pages-page-2']['participants-container']['participants'][$user_id]['name'] = [
+          '#type' => 'html_tag',
+          '#tag' => 'div',
+          '#value' => $user->label(),
+          '#attributes' => [
+            'class' => ['col-xs-6'],
+          ],
+        ];
+        $form['pages-page-2']['participants-container']['participants'][$user_id]['internal'] = [
+          '#type' => 'checkbox',
+          '#prefix' => '<div class="col-xs-3">',
+          '#suffix' => '</div>'
+        ];
+        $form['pages-page-2']['participants-container']['participants'][$user_id]['external'] = [
+          '#type' => 'checkbox',
+          '#prefix' => '<div class="col-xs-3">',
+          '#suffix' => '</div>'
+        ];
+      }
+    }
+    // Participants END.
 
     return $form;
   }
@@ -212,10 +412,14 @@ class MeetingsEditForm extends FormBase {
     $location_tid = $form_state->getValue('location');
 
     // Participants.
-    $populate_department_members = $form_state->getValue('populate_department_members');
+    $autofill_participants = $form_state->get('autofill_participants');
     $field_decreto_meet_partic_int = [];
-    if ($populate_department_members) {
+    $field_decreto_meet_partic_ext = [];
+
+    if ($autofill_participants) {
+      // Fill participants array based on user department attribute.
       $query = \Drupal::entityQuery('user')
+        ->condition('status', 1)
         ->condition('field_decreto_usr_departments', $department_tid, 'IN');
       $users_ids = $query->execute();
       if (!empty($users_ids)) {
@@ -224,7 +428,19 @@ class MeetingsEditForm extends FormBase {
         }
       }
     }
+    else {
+      // Not autofill, grab the selected participants.
+      $participants = $form_state->getValue('participants');
 
+      foreach($participants as $user_id => $participant) {
+        if ($participant['internal']) {
+          $field_decreto_meet_partic_int[]['target_id'] = $user_id;
+        }
+        if ($participant['external']) {
+          $field_decreto_meet_partic_ext[]['target_id'] = $user_id;
+        }
+      }
+    }
 //    $description = $form_state->getValue('description');
 //    $full_doc = $form_state->getValue('full_doc');
 //    $full_doc_closed = $form_state->getValue('full_doc_closed');
@@ -240,6 +456,7 @@ class MeetingsEditForm extends FormBase {
         'field_decreto_meet_start_date' => ($start_date) ? $start_date->format(DateTimeItemInterface::DATETIME_STORAGE_FORMAT) : NULL,
         'field_decreto_meet_end_date' => ($end_date) ? $end_date->format(DateTimeItemInterface::DATETIME_STORAGE_FORMAT) : NULL,
         'field_decreto_meet_partic_int' => $field_decreto_meet_partic_int,
+        'field_decreto_meet_partic_ext' => $field_decreto_meet_partic_ext,
 //        'field_decreto_meet_full_doc' => !empty($full_doc) ? ['target_id' => array_pop($full_doc)] : NULL,
 //        'field_decreto_meet_full_doc_c' => !empty($full_doc_closed) ? ['target_id' => array_pop($full_doc_closed)] : NULL,
       ]);
@@ -259,6 +476,48 @@ class MeetingsEditForm extends FormBase {
 //    }
 //
     $this->meeting->save();
+  }
+
+  /**
+   * Submit handler for 'populate_department_members' button.
+   *
+   * Toggles between participants autofill and manual fill.
+   *
+   * @param array $form
+   *   Render array representing from.
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   *   Current form state.
+   */
+  public function submitToggleParticipantsAutofill(array &$form, FormStateInterface $form_state) {
+    $autofillParticipants = $form_state->get('autofill_participants');
+
+    $form_state->set('autofill_participants', !$autofillParticipants);
+    $form_state->setRebuild();
+  }
+
+  /**
+   * Submit handler for 'swift-page' button.
+   *
+   * Switches between pages.
+   *
+   * @param array $form
+   *   Render array representing from.
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   *   Current form state.
+   */
+  public function submitSwitchPage(array &$form, FormStateInterface $form_state) {
+    // Getting current page.
+    $active_page = $form_state->get('active_page');
+
+    // Switching page.
+    if ($active_page === 1) {
+      $form_state->set('active_page', 2);
+    }
+    else {
+      $form_state->set('active_page', 1);
+    }
+
+    $form_state->setRebuild();
   }
 
   /**
@@ -306,6 +565,34 @@ class MeetingsEditForm extends FormBase {
       $response->addCommand(new CloseModalDialogCommand());
       $response->addCommand(new RedirectCommand($this->meeting->toUrl()->toString()));
     }
+
+    return $response;
+  }
+
+  /**
+   * Replaces the form after it has been reloaded by submit handler.
+   *
+   * @param array $form
+   *   Render array representing from.
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   *   Current form state.
+   *
+   * @return \Drupal\Core\Ajax\AjaxResponse
+   *   Array of ajax commands to execute on submit of the modal form.
+   */
+  public function ajaxReloadForm(array &$form, FormStateInterface $form_state) {
+    $response = new AjaxResponse();
+
+    if ($form_state->getErrors()) {
+      // Replacing form to show errors.
+      $form['status_messages'] = [
+        '#type' => 'status_messages',
+        '#weight' => -10,
+      ];
+    }
+
+    // Rebuilding form after ajax request.
+    $response->addCommand(new ReplaceCommand('#' . $this->getFormId(), $form));
 
     return $response;
   }
