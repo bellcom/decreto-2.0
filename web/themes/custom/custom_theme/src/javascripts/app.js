d814160e0bf34151e5dddfea9c2d8464b28d95b5
Morten Nissen <mail@hjemmesidekongen.dk>
2019-08-15 13:56:10 +0200

    Remove unused javascript

--- web/themes/custom/custom_theme/src/javascripts/app.js
+++ web/themes/custom/custom_theme/src/javascripts/app.js
@@ -1,88 +1,58 @@
-jQuery(function($) {
-    'use strict';
+jQuery(function ($) {
+  'use strict';
 
-    // Flexy header
-    flexy_header.init();
+  // Flexy header
+  flexy_header.init();
 
-    $('.sidr-toggle--right').sidr({
-        name: 'sidr-main',
-        side: 'right',
-        renaming: false,
-        body: '.layout__wrapper',
-        source: '.sidr-source-provider'
-    });
+  $('.sidr-toggle--right').sidr({
+    name: 'sidr-main',
+    side: 'right',
+    renaming: false,
+    body: '.layout__wrapper',
+    source: '.sidr-source-provider'
+  });
 
-    // Enable / disable Bootstrap tooltips, based upon touch events
-    if(Modernizr.touchevents) {
-        $('[data-toggle="tooltip"]').tooltip('hide');
-    }
-    else {
-        $('[data-toggle="tooltip"]').tooltip();
-    }
+  // Enable / disable Bootstrap tooltips, based upon touch events
+  if (Modernizr.touchevents) {
+    $('[data-toggle="tooltip"]').tooltip('hide');
+  }
+  else {
+    $('[data-toggle="tooltip"]').tooltip();
+  }
 
-    // Poppy (popovers).
-    $('.poppy-toggle').on('click', function(event) {
-        var $element = $(this);
-        var $parent = $element.parents('.poppy');
+  // Poppy (popovers).
+  $('.poppy-toggle').on('click', function (event) {
+    var $element = $(this);
+    var $parent = $element.parents('.poppy');
 
-        // Make sure that no other "poppys" are open.
-        $('.poppy--open')
-            .not($parent)
-            .removeClass('poppy--open');
+    // Make sure that no other "poppys" are open.
+    $('.poppy--open')
+      .not($parent)
+      .removeClass('poppy--open');
 
-        // Toggle the class on this element.
-        $parent.toggleClass('poppy--open');
-    });
-    $('.poppy').on('click', function(event) {
-       event.stopPropagation();
-    });
-    $('body').on('click', function(event) {
-        $('.poppy--open').removeClass('poppy--open');
-    });
+    // Toggle the class on this element.
+    $parent.toggleClass('poppy--open');
+  });
+  $('.poppy').on('click', function (event) {
+    event.stopPropagation();
+  });
+  $('body').on('click', function (event) {
+    $('.poppy--open').removeClass('poppy--open');
+  });
 
-    // Ajaxi click loader.
-    $('[data-ajaxi-source]').on('click', function(event) {
-        var $element = $(this);
-        var target = $element.attr('data-ajaxi-target');
-        var source = $element.attr('data-ajaxi-source');
-        var loading = $element.attr('data-ajaxi-loading');
+  // Ajaxi click loader.
+  $('[data-ajaxi-source]').on('click', function (event) {
+    var $element = $(this);
+    var target = $element.attr('data-ajaxi-target');
+    var source = $element.attr('data-ajaxi-source');
+    var loading = $element.attr('data-ajaxi-loading');
 
-        // Set loading text.
-        $(target).html(loading);
+    // Set loading text.
+    $(target).html(loading);
 
-        // Load external content.
-        $(target).load(source);
-    });
-
-    // Meeting agenda bulletpoints collapse toggle callback.
-    $('#meeting-agenda-collapse-toggle').on('click', function(event) {
-        // If we have any closed BP containers, open ALL first.
-        if ($('article.decreto-bullet-point > .area-expand-collapse:not(.in)').length) {
-          $('article.decreto-bullet-point > .area-expand-collapse').collapse('show');
-        }
-        // All BP containers are open, close ALL.
-        else {
-          $('article.decreto-bullet-point > .area-expand-collapse').collapse('hide');
-        }
-        event.preventDefault();
-    });
-
-    // Bullet point collapse toggle callback.
-    $('.bullet-point-toggle').on('click', function(event) {
-        var $wrapper = $(this).parents('article:first');
-        // Opening BP container.
-        $wrapper.find('> .area-expand-collapse').collapse('show');
-
-        // If we have any closed BPA containers, open ALL first.
-        if ($wrapper.find('article.decreto-bullet-point-attachment > .area-expand-collapse:not(.in)').length) {
-          $wrapper.find('article.decreto-bullet-point-attachment > .area-expand-collapse').collapse('show');
-        }
-        // All BPA containers are open, close ALL.
-        else {
-          $wrapper.find('article.decreto-bullet-point-attachment > .area-expand-collapse').collapse('hide');
-        }
-        event.preventDefault();
-    });
+    // Load external content.
+    $(target).load(source);
+  });
 
   // Switch mode toggle callback.
   $('#meeting-agenda-switch-mode-toggle').on('click', function (event) {
