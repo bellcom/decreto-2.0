3a7f159be791835925afb523d4c901b845bed067
Stanislav Kutasevits <stan@bellcom.dk>
2019-09-06 16:43:09 +0300

    DE-173 Organisation switcher

--- web/modules/custom/decreto_organisation/src/Controller/OrganisationController.php
+++ web/modules/custom/decreto_organisation/src/Controller/OrganisationController.php
@@ -2,7 +2,11 @@
 
 namespace Drupal\decreto_organisation\Controller;
 
+use Drupal\Core\Cache\Cache;
 use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Url;
+use Drupal\node\NodeInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
 
 /**
@@ -13,8 +17,8 @@ class OrganisationController extends ControllerBase {
   /**
    * Renders view display.
    *
-   * Renders "decreto_organisation_popup_embed" display of "
-   * decreto_organisations" view and returns the result.
+   * Renders "decreto_organisation_popup_embed" display of
+   * "decreto_organisations" view and returns the result.
    *
    * @return \Symfony\Component\HttpFoundation\Response
    *   Rendered view result.
@@ -25,6 +29,28 @@ class OrganisationController extends ControllerBase {
 
     // This is the important part, because will render only the TWIG template.
     return new Response($markup);
+  }
+
+  /**
+   * Switching user selected organisation.
+   *
+   * @param \Drupal\node\NodeInterface $decreto_organisation
+   *   New selected organisation.
+   *
+   * @return \Symfony\Component\HttpFoundation\RedirectResponse
+   *   Redirect to the front page.
+   */
+  public function switchOrganisation(NodeInterface $decreto_organisation) {
+    $tempstore = \Drupal::service('user.private_tempstore')->get('decreto_organisation');
+    $tempstore->set('selected_organisation', $decreto_organisation->id());
+
+    // Invalidating user tags.
+    Cache::invalidateTags(array('user:' . \Drupal::currentUser()->id()));
+
+    $url = Url::fromRoute('<front>');
+    $response = new RedirectResponse($url->toString());
+
+    return $response;
   }
 
 }
