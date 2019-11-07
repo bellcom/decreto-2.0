b7781bb18b726292b99f7be77c183cbd663dd367
Stanislav Kutasevits <stan@bellcom.dk>
2019-11-07 13:58:21 +0200

    Refactor, making AjaxDeleteForm usable for terms

--- web/modules/custom/decreto_content_modify/src/Form/BulletPointAttachmentDeleteForm.php
+++ web/modules/custom/decreto_content_modify/src/Form/BulletPointAttachmentDeleteForm.php
@@ -2,9 +2,9 @@
 
 namespace Drupal\decreto_content_modify\Form;
 
+use Drupal\Core\Entity\ContentEntityInterface;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\decreto_content_modify\Entity\DecretoBulletPointAttachment;
-use Drupal\node\NodeInterface;
 
 /**
  * Class BulletPointAttachmentDeleteForm.
@@ -33,13 +33,13 @@ class BulletPointAttachmentDeleteForm extends AjaxDeleteFormBase {
   /**
    * {@inheritdoc}
    */
-  public function buildForm(array $form, FormStateInterface $form_state, NodeInterface $node = NULL) {
+  public function buildForm(array $form, FormStateInterface $form_state, ContentEntityInterface $bullet_point_attachment = NULL) {
     // Saving meeting for redirect purposes.
-    $decretoBPA = new DecretoBulletPointAttachment($node);
+    $decretoBPA = new DecretoBulletPointAttachment($bullet_point_attachment);
     $meeting = $decretoBPA->getMeeting();
     $this->parent = $meeting;
 
-    return parent::buildForm($form, $form_state, $node);
+    return parent::buildForm($form, $form_state, $bullet_point_attachment);
   }
 
 }
