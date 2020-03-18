3ee94234c27f326b942de8d8feb91a4dcef98c4b
Stanislav Kutasevits <stan@bellcom.dk>
2020-03-18 16:13:02 +0200

    DE-304 Notifications counter does not ignore read messages

--- web/modules/custom/decreto_notification/src/Plugin/Notifier/NotificationNotifier.php
+++ web/modules/custom/decreto_notification/src/Plugin/Notifier/NotificationNotifier.php
@@ -2,11 +2,14 @@
 
 namespace Drupal\decreto_notification\Plugin\Notifier;
 
+use Drupal\decreto_content_modify\Entity\DecretoMeeting;
+use Drupal\decreto_department\Entity\DecretoDepartment;
 use Drupal\decreto_notification\Entity\Notification;
 use Drupal\message_notify\Plugin\Notifier\MessageNotifierBase;
 
 /**
  * Decreto notifier.
+ *
  * @Notifier(
  *   id = "decreto_notifier",
  *   title = @Translation("Decreto notifier"),
@@ -16,6 +19,7 @@ use Drupal\message_notify\Plugin\Notifier\MessageNotifierBase;
  * )
  */
 class NotificationNotifier extends MessageNotifierBase {
+
   /**
    * {@inheritdoc}
    */
@@ -25,10 +29,18 @@ class NotificationNotifier extends MessageNotifierBase {
       $text = reset($text);
     }
 
+    // Getting organisation id.
+    $meeting = $this->message->get('field_decreto_notif_meeting')->first()->entity;
+    $decretoMeeting = new DecretoMeeting($meeting);
+    $department = $decretoMeeting->getDepartment();
+    $decretoDepartment = new DecretoDepartment($department);
+    $organisationId = $decretoDepartment->getOrganisation(FALSE);
+
     return Notification::create([
       'body' => strip_tags($text),
       'uid' => $this->message->getOwnerId(),
-      'mid' => $this->message->original_message->id()
+      'mid' => $this->message->original_message->id(),
+      'org_id' => $organisationId,
     ])->save();
   }
 
